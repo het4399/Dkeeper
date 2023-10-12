47a7ebe1ee4f806f13d0af462f285aecf79deaed
Het4399 <het.sojitra4399@gmail.com>
2023-10-12 16:35:00 +0530

    SetupFile

---
+++ src/dkeeper_frontend/src/Components/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+  function deleteNote(id){
+    setNotes(prevNotes=>{
+     return prevNotes.filter((noteitem,index)=>{
+        return index !==id;
+      })
+    })
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteitem,index) => (
+        <Note
+        key={index}
+        id={index}
+          title={noteitem.title}
+          content={noteitem.content}
+         onDelete={deleteNote}/>
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
